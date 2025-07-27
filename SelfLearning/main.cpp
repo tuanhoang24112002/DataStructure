1b5b5558489222317f825aa8373c4500c3e2708c
HoangTuan <tuanhoangnguyen24112002@gmail.com>
2025-07-27 16:52:40 +0700

    thread and mutex learning 2025/07/27

--- SelfLearning/main.cpp
+++ SelfLearning/main.cpp
@@ -15,11 +15,41 @@ void say_hello() {
     this_thread::sleep_for(chrono::seconds(1)); // Simulate some work
 }
 
+void count_even(int startNum, int endNun)
+{
+    int sum_even = 0;
+    for (int i = startNum; i <= endNun; i++) {
+        if (i % 2 == 0) {
+            sum_even += i;
+        }
+    }
+    cout << "Sum of even numbers: " << sum_even << endl;
+}
+
+void count_odd(int startNum, int endNun)
+{
+    int sum_odd = 0;
+    for (int i = startNum; i <= endNun; i++) {
+        if (i % 2 != 0) {
+            sum_odd += i;
+        }
+    }
+    cout << "Sum of odd numbers: " << sum_odd << endl;
+}
+
 int main(){
-    
-    thread t(say_hello);
-    thread t1(say_hello);
-    t.join(); // Wait for the thread to finish
-    t1.join(); // Wait for the second thread to finish
+    auto start = chrono::high_resolution_clock::now();
+
+    // thread t1(count_even, 1, 1000000);
+    // thread t2(count_odd, 1, 1000000);
+    // t1.join();
+    // t2.join();
+    count_even(1, 1000000);
+    count_odd(1, 1000000);
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    cout << "Total execution time: " << elapsed.count() << " seconds" << endl;
+
     return 0;
 }
